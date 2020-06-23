3b5ac8dfe7f46421a85f378ebfda856cdf607e2c
Ilia Polozov <uber84@bk.ru>
2020-06-23 22:11:05 +0200

    change api endpoint

--- app/src/main/java/com/onza/barcode/app/Constants.kt
+++ app/src/main/java/com/onza/barcode/app/Constants.kt
@@ -1,5 +1,6 @@
 package com.onza.barcode.app
 
 object Constants {
-    val BASE_URL = "http://project003.dev.onza.me/api/"
+//    val BASE_URL = "http://project003.dev.onza.me/api/"
+    val BASE_URL = "http://goods-dev.rtgsoft.ru/"
 }
