654f1fbba90075432333a624097c9103e3445665
Ilia Polozov <uber84@bk.ru>
2020-06-25 12:42:09 +0200

    fixed endpoint url

--- app/src/main/java/com/onza/barcode/app/Constants.kt
+++ app/src/main/java/com/onza/barcode/app/Constants.kt
@@ -2,5 +2,5 @@ package com.onza.barcode.app
 
 object Constants {
 //    val BASE_URL = "http://project003.dev.onza.me/api/"
-    val BASE_URL = "http://goods-dev.rtgsoft.ru/"
+    val BASE_URL = "http://goods-dev.rtgsoft.ru/api/"
 }
